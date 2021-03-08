4c8fbcfada9260a53a05d3b72a644f0198157936
Yabe.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2021-03-08 15:33:41 +0900

    Refactor Makefile

--- kernel/makedef-aarch64.mk
+++ kernel/makedef-aarch64.mk
@@ -1,17 +1,34 @@
-ARCH_DIR            := arch/aarch64
+MIKANOS_ARCH_DEFINE := ARCH_AARCH64
+MIKANOS_ARCH_TARGET := aarch64-linux-gnu
 MIKANOS_KERNEL_BASE := 0x40000000
-CLANG_SYSROOT       := --sysroot=$(SYSROOT)
 
-AS                  := clang
-ASFLAGS             := --target=$(CLANG_TARGET)
-#CCLD                := clang
-#CCLD_FLAGS          := -nostdlib -fuse-ld=lld -target aarch64-linux-gnu $(CLANG_SYSROOT) $(addprefix -Xlinker ,$(LDFLAGS))
-CCLD                := ld.lld
-CCLD_FLAGS          := $(CLANG_SYSROOT) $(LDFLAGS)
+CLANG_SYSROOT       := $(SYSROOT)
 
-ARCH_LIBS           := -L$(SYSROOT)/lib/gcc/aarch64-none-linux-gnu/10.2.1/ -lgcc
-ARCH_OBJS           := $(ARCH_DIR)/iofunc.o $(ARCH_DIR)/pci.o $(ARCH_DIR)/halt.o $(ARCH_DIR)/libcxx_support.o
-LIBS                := -lc -lc++ -lc++abi -lm -lunwind
-OBJCOPY_TARGET      := elf64-aarch64
-EDK2_ARCH           := AARCH64
+CLANG_VERSION       := -11
+CPP                 := clang-cpp$(CLANG_VERSION)
+CC                  := clang$(CLANG_VERSION)
+CXX                 := clang++$(CLANG_VERSION)
+LD                  := ld.lld$(CLANG_VERSION)
+OBJCOPY             := llvm-objcopy$(CLANG_VERSION)
+AS                  := $(CC)
+
+ARCH_INC_PATH       := -I$(KERNEL_DIR) \
+			-I$(KERNEL_DIR)/arch/$(MIKANOS_ARCH_TARGET) \
+			-I$(DEVENV_DIR)/$(MIKANOS_ARCH_TARGET)/include/c++/v1 \
+			-I$(DEVENV_DIR)/$(MIKANOS_ARCH_TARGET)/include \
+			-I$(DEVENV_DIR)/$(MIKANOS_ARCH_TARGET)/include/freetype2
+ARCH_LIB_PATH       := -L$(DEVENV_DIR)/$(MIKANOS_ARCH_TARGET)/lib \
+			-L$(SYSROOT)/lib/gcc/aarch64-none-linux-gnu/10.2.1/
+
+ARCH_CPPFLAGS       := --target=$(MIKANOS_ARCH_TARGET) $(ARCH_INC_PATH) -D$(MIKANOS_ARCH_DEFINE)=1
+ARCH_CFLAGS         := --target=$(MIKANOS_ARCH_TARGET) $(ARCH_INC_PATH) -D$(MIKANOS_ARCH_DEFINE)=1
+ARCH_CXXFLAGS       := --target=$(MIKANOS_ARCH_TARGET) $(ARCH_INC_PATH) -D$(MIKANOS_ARCH_DEFINE)=1
+ARCH_LDFLAGS        := --image-base $(MIKANOS_KERNEL_BASE) $(ARCH_LIB_PATH)
+ARCH_ASFLAGS        := --target=$(MIKANOS_ARCH_TARGET)
+ARCH_OBJCOPYFLAGS   := -O elf64-aarch64
+
+ARCH_OBJS           := iofunc.o pci.o halt.o libcxx_support.o
+ARCH_LIBS           := -lc++abi -lm -lunwind -lgcc
+
+EDK2_ARCH_TARGET    := AARCH64
 
