4bd8963471e6f963d3fffcf060ffc19322b1f144
YABE.Kazuhiro <Kazuhiro.Yabe@gmail.com>
2021-12-03 17:32:21 +0900

    Add functions to access with 8, 16, 64 bit

--- kernel/arch/aarch64-linux-gnu/iofunc.cpp
+++ kernel/arch/aarch64-linux-gnu/iofunc.cpp
@@ -19,6 +19,42 @@
 extern int printk(const char* format, ...);
 
 extern "C" {
+    void io_write8(uint64_t address, uint8_t data)
+    {
+        // write_memory_barrier();
+        //printk("address:%0p\n", address);
+        volatile uint8_t *adrs = (volatile uint8_t *)address;
+        *adrs = data;
+    }
+
+    uint8_t io_read8(uint64_t address)
+    {
+        //printk("read:%0p\n", address);
+        volatile uint8_t *adrs = (volatile uint8_t *)address;
+        uint16_t data = *adrs;
+        //printk("data:%02x\n", data);
+        // read_memory_barrier();
+        return data;
+    }
+
+    void io_write16(uint64_t address, uint16_t data)
+    {
+        // write_memory_barrier();
+        //printk("address:%0p\n", address);
+        volatile uint16_t *adrs = (volatile uint16_t *)address;
+        *adrs = data;
+    }
+
+    uint16_t io_read16(uint64_t address)
+    {
+        //printk("read:%0p\n", address);
+        volatile uint16_t *adrs = (volatile uint16_t *)address;
+        uint16_t data = *adrs;
+        //printk("data:%04x\n", data);
+        // read_memory_barrier();
+        return data;
+    }
+
     void io_write32(uint64_t address, uint32_t data)
     {
         // write_memory_barrier();
@@ -33,6 +69,24 @@ extern "C" {
         volatile uint32_t *adrs = (volatile uint32_t *)address;
         uint32_t data = *adrs;
         //printk("data:%08x\n", data);
+        // read_memory_barrier();
+        return data;
+    }
+
+    void io_write64(uint64_t address, uint64_t data)
+    {
+        // write_memory_barrier();
+        //printk("address:%0p\n", address);
+        volatile uint64_t *adrs = (volatile uint64_t *)address;
+        *adrs = data;
+    }
+
+    uint64_t io_read64(uint64_t address)
+    {
+        //printk("read:%0p\n", address);
+        volatile uint64_t *adrs = (volatile uint64_t *)address;
+        uint32_t data = *adrs;
+        //printk("data:%016x\n", data);
         // read_memory_barrier();
         return data;
     }
--- kernel/arch/aarch64-linux-gnu/iofunc.hpp
+++ kernel/arch/aarch64-linux-gnu/iofunc.hpp
@@ -16,8 +16,19 @@
 
 
 extern "C" {
+    #include <cstdint>
+
+    void io_write8(uint64_t adrs, uint8_t data);
+    uint8_t io_read8(uint64_t adrs);
+
+    void io_write16(uint64_t adrs, uint16_t data);
+    uint16_t io_read16(uint64_t adrs);
+
     void io_write32(uint64_t adrs, uint32_t data);
     uint32_t io_read32(uint64_t adrs);
+
+    void io_write64(uint64_t adrs, uint64_t data);
+    uint64_t io_read64(uint64_t adrs);
 }
 
 #endif /* __IOFUNC_H__ */
